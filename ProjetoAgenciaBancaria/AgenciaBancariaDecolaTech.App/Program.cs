3385d28b85a8696fa7e97382ddcffe4b36c722f9
KalebeSantana <santana.abrantes01@gmail.com>
2021-10-13 14:09:26 -0300

    Primeira mentoria

---
+++ ProjetoAgenciaBancaria/AgenciaBancariaDecolaTech.App/Program.cs
@@ -0,0 +1,34 @@
+﻿using AgenciaBancariaDecolaTech.Dominio;
+using System;
+
+namespace AgenciaBancariaDecolaTech.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("rua teste", "1847528", "Guarulhos", "SP");
+                Cliente cliente = new Cliente("Kalebe", "234131", "1241892673498", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta: " + conta.Situacao + ": " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                string senha = "abc12321342";
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta: " + conta.Situacao + ": " +conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo: " + conta.Saldo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ProjetoAgenciaBancaria/AgenciaBancariaDecolaTech.Dominio/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancariaDecolaTech.Dominio
+{
+    public class ContaCorrente : ContaBancaria
+    {
+        public ContaCorrente(Cliente cliente, decimal limite) : base(cliente)
+        {
+            ValorTaxaAmnutencao = 0.05M;
+            Limite = limite;
+        }
+
+        public override void Sacar(decimal valor, string senha)
+        {
+            if (Senha != senha)
+            {
+                throw new Exception("Senha inválida.");
+            }
+            if ((Saldo + Limite) < valor)
+            {
+                throw new Exception("Saldo insuficiente.");
+            }
+
+            Saldo -= valor;
+        }
+
+        public decimal Limite { get; private set; }
+        public decimal ValorTaxaAmnutencao { get; private set; }
+    }
+}
